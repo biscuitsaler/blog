70913e4013c610724cd3db02b1dcf906cd0813ae
biscuitsaler <durandmallory@live.fr>
2017-01-05 11:15:08 +0100

    ajout fonctions

--- classes/users.php
+++ classes/users.php
@@ -15,4 +15,21 @@ class User {
     $this->firstname = $firstname;
     }
 }
+ public static function verifierEmail($value){
+        $value = strtolower(trim(strip_tags($value)));
+        if(!preg_match("/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}$/", $value)){
+            return false;
+        }
+        return true;
+ }
+ public static function verifierPwd($value){
+        $value = strtolower(trim(strip_tags($value)));
+        if(mb_strlen($value) < 4){
+            return false;
+        }
+        return true;
+    }
+    public static function crypterPwd($value){
+        return md5($value);
+    }
 ?>
